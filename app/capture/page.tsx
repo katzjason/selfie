9e0101f778146a911489ad38b84eabd96b94bd4a
Katz <katzj2@mskcc.org>
2026-02-17 19:34:22 -0500

    Render camera if stream AND not loading

--- app/capture/page.tsx
+++ app/capture/page.tsx
@@ -521,7 +521,7 @@ export default function Capture() {
 
 
 
-      {!!stream && <main className="flex-1 flex flex-col p-2 w-full mx-auto overflow-hidden bg-black justify-center pb-4">
+      {stream && !isLoadingCamera &&  <main className="flex-1 flex flex-col p-2 w-full mx-auto overflow-hidden bg-black justify-center pb-4">
       {/* Instruction bar at top */}
       <div className="mb-2 rounded-xl bg-black/70 text-white px-3 py-2 flex items-center justify-between">
         <div className="flex flex-col">
